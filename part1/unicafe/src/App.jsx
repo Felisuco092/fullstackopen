1cee7da2d0b67ae2499b1b953afbc8f1ddab55c8
Felisuco092 <felisuco146@gmail.com>
2025-06-29 18:12:52 +0200

    Exercise 1.10 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,12 @@ const Button = ({text, onClick}) => {
   )
 }
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good, neutral, bad}) => {
   const all = good+neutral+bad
   if(all === 0) {
@@ -15,12 +21,12 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return (
     <>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {all}</p>
-    <p>average {(good-bad)/all}</p>
-    <p>positive {good*100/all}%</p>
+    <StatisticLine text="good" value ={good} />
+    <StatisticLine text="neutral" value ={neutral} />
+    <StatisticLine text="bad" value ={bad} />
+    <StatisticLine text="all" value ={all} />
+    <StatisticLine text="average" value ={(good-bad)/all} />
+    <StatisticLine text="positive" value ={good*100/all + "%"} />
     </>
   )
 }
