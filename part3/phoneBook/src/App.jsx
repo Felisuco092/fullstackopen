137b86733beb0cc958bd494102568170d6b1c72a
Felisuco092 <felisuco146@gmail.com>
2025-07-05 19:34:36 +0200

    phoneBook added to  part3 for changes in this part

---
+++ part3/phoneBook/src/App.jsx
@@ -0,0 +1,178 @@
+import { useState, useEffect } from 'react'
+import notesManagement from './services/notes'
+
+const NotificationSuccess = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="success">
+      {message}
+    </div>
+  )
+}
+
+const NotificationError = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const Person = ({name, number, handleDelete, id}) => {
+  return (
+    <>
+      {name} {number}
+      <button onClick={handleDelete(name, id)}>delete</button>
+      <br />
+    </>
+  )
+}
+
+const Filter = ({filter, setFilter}) => {
+  return (
+    <div>
+        filter shown with<input value={filter} onChange={(event) => setFilter(event.target.value)}/>
+    </div>
+  )
+}
+
+const PersonForm = ({handleSubmit, newName, setNewName, number, setNumber}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName}
+            onChange={(event) => 
+            setNewName(event.target.value)}/>
+        </div>
+        <div>number: <input value={number}
+            onChange={(event) => 
+            setNumber(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Persons = ({filterObject, handleDelete}) => {
+  return (
+    <>
+      {filterObject.map((person) => <Person 
+        key={person.name} name={person.name} number={person.number}
+        handleDelete={handleDelete} id={person.id}/>)}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [number, setNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
+  useEffect(() => {
+    notesManagement
+      .getAll()
+      .then((data) => {
+        setPersons(data)
+      })
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const message = `${newName} is already added to phonebook, replace the old with a new one?`
+    if(persons.some((person) => person.name === newName )) {
+      if(confirm(message)) {
+        const personFind = persons.find((person) => person.name === newName)
+        const newPerson = {...personFind, number: number}
+        notesManagement
+          .update(personFind.id, newPerson)
+          .then((serverResponse) => {
+            const indexPerson = persons.findIndex(person => person.id === personFind.id)
+            const copyPersons = [...persons]
+            copyPersons[indexPerson] = newPerson
+            setPersons(copyPersons)
+          })
+          .catch((error) => {
+            setErrorMessage(`Information of ${newName} has already been removed from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            },3000)
+            setPersons(persons.filter((person) => person.name !== newName))
+          })
+      }
+    } else {
+      const newPerson = { name: newName, number: number}
+      notesManagement
+        .create(newPerson)
+        .then(serverResponse =>  {
+          setPersons(persons.concat(serverResponse))
+          setSuccessMessage(
+          `Added ${newName}`
+          )
+          setTimeout(() => {
+            setSuccessMessage(null)
+          }, 5000)
+        })
+    }
+    setNewName('')
+    setNumber('')
+  }
+
+
+  const handleDelete = (name, id) => {
+    return () => {
+      if(confirm(`Delete ${name} ?`)) {
+        notesManagement
+          .deleteNote(id)
+          .then(deletedNote => {
+            setPersons(persons.filter(person => person.id !== id))
+          })
+          .catch((error) => {
+            setPersons(persons.filter((person) => person.name !== name))
+          })
+      }
+    }
+  }
+  
+  
+  
+  
+  const filterObject = persons.filter(person => {
+        return person.name.toLowerCase().includes(filter.toLowerCase())
+      })
+
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <NotificationSuccess message={successMessage} />
+      <NotificationError message={errorMessage} />
+      <Filter filter={filter} setFilter={setFilter}/>
+      <h2>add a new</h2>
+      <PersonForm 
+      handleSubmit={handleSubmit}
+      newName={newName}
+      setNewName={setNewName}
+      number={number}
+      setNumber={setNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons filterObject={filterObject} handleDelete={handleDelete}/>
+    </div>
+  )
+}
+
+export default App
---
+++ part3/phoneBook/src/services/notes.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = (data) => {
+    const request = axios.post(baseUrl, data)
+    return request.then(response => response.data)
+}
+
+const deleteNote = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const update = (id, changedNote) => {
+    const request = axios.put(`${baseUrl}/${id}`, changedNote)
+    return request.then(response => response.data)
+}
+export default { getAll, create, deleteNote, update }
