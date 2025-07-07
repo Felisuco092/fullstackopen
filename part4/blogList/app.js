2df943670c01e2b487fb4537d59432346f8b04bc
Felisuco092 <felisuco146@gmail.com>
2025-07-08 00:55:29 +0200

    Exercise 4.1 and 4.2, i refactored in modules without reading the exercise 4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGO_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ part4/blogList/index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+const config = require('./utils/config')
+const { info, error } = require('./utils/logger')
+
+const PORT = config.PORT
+app.listen(PORT, () => {
+  info(`Server running on port ${PORT}`)
+})
