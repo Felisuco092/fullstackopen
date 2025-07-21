4ae02e825a8dcbcb63e946c77ac5d350e39c874e
Felisuco092 <felisuco146@gmail.com>
2025-07-21 17:11:43 +0200

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [show, setShow] = useState(false)
+
+  const changeView = () => {
+    setShow(!show)
+  }
+  if(show) {
+    return (
+      <div style={blogStyle}>
+        {blog.title}<button onClick={changeView}>hide</button> <br/>
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button> <br />
+        {blog.author} <br />
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={changeView}>view</button>
+      </div>  
+    )
+  }
+}
 
 export default Blog
