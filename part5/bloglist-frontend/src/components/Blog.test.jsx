046dec72e722469aaa596d959ac480b045539780
Felisuco092 <felisuco146@gmail.com>
2025-07-23 18:01:29 +0200

    Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,15 +1,34 @@
 import Blog from './Blog'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('<Blog />', () => {
-    test('First render blog shows title and author but not the url or likes', async () => {
-        render(<Blog blog={ {title: 'React blog', author: 'Felix', likes: 5, url: 'https://ey.com'} }/>)
+    test('First render blog shows title and author but not the url or likes', () => {
+        const blog = {title: 'React blog', author: 'Felix', likes: 5, url: 'https://ey.com', user:{username:"Root"}}
+        render(<Blog blog={ blog }/>)
 
         const element = screen.getByText('React blog Felix')
-        const url = await screen.queryByText('https://ey.com')
-        const likes = await screen.queryByText('5')
+        const url = screen.queryByText('https://ey.com')
+        const likes = screen.queryByText('5')
         expect(element).toBeDefined()
         expect(url, likes).toBeNull()
+    })
+
+    test('When pressed view button, the url and likes are displayed', async () => {
+        const blog = {title: 'React blog', author: 'Felix', likes: 5, url: 'https://ey.com', user:{username:"Root"}}
+        const container = render(<Blog blog={ blog }/>).container
+
+        const user = userEvent.setup()
+
+        const view = screen.getByText('view')
+        await user.click(view)
+
+        const url = screen.getByText('https://ey.com', { exact: false })
+        const like = screen.getByText('5', { exact: false })
+
+        expect(url).toBeDefined()
+        expect(like).toBeDefined()
+
     })
     
 })
